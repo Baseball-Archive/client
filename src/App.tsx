24a5f1ac6a89d33e752742358e9ccfb056b4f580
ParkJeongJoon <parkjeong616@gmail.com>
2024-08-10 02:34:25 +0900

    ⚡️  라우터, 리액트쿼리 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <>
-      <div className="flex justify-center text-8xl">HI</div>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route>
+            <div className="flex justify-center text-8xl">HI</div>
+          </Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
