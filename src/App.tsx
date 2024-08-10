02812c8f01efec031b825410a481abe56906692b
ParkJeongJoon <parkjeong616@gmail.com>
2024-08-10 20:20:28 +0900

    ⚡️ feat reactquerydevtools 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      throwOnError: true,
+    },
+    mutations: {
+      throwOnError: true,
+    },
+  },
+});
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
