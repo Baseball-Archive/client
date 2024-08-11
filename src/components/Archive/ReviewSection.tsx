48958d397cb9b11a8e9f9c02c4d3e1f86626d0d6
ParkJeongJoon <parkjeong616@gmail.com>
2024-08-12 01:00:26 +0900

    :sparkles: Home 구현

---
+++ src/components/Archive/ReviewSection.tsx
@@ -0,0 +1,26 @@
+interface ReviewSectionProps {
+  review: string;
+  result: string;
+  isExpanded: boolean;
+  onToggleExpand: () => void;
+}
+
+const ReviewSection: React.FC<ReviewSectionProps> = ({
+  review,
+  result,
+  isExpanded,
+  onToggleExpand,
+}) => (
+  <div className="w-full">
+    <p>{result}</p>
+    <p
+      className={`text-gray-700 ${isExpanded ? "line-clamp-none" : "line-clamp-1"}`}
+    >
+      {review}
+    </p>
+    <button onClick={onToggleExpand} className="mt-5 text-sm text-gray-500">
+      {isExpanded ? "" : "...더 보기"}
+    </button>
+  </div>
+);
+export default ReviewSection;
