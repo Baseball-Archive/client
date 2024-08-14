ab60e9b241c50dee0b56452fb6b4894a4ba3f616
harry <tl9434@naver.com>
2024-08-14 17:37:30 +0900

    🐛 리팩토링

--- src/components/Community/Post.tsx
+++ src/components/Community/Post.tsx
@@ -10,34 +10,21 @@ type TeamScheme =
   | "lotte"
   | "kiwoom";
 
-const teamColors: { [key in TeamScheme]: string } = {
-  kia: "#440011",
-  samsung: "#1D67B2",
-  lg: "#C22D40",
-  doosan: "#012561",
-  ssg: "#65615E",
-  kt: "#000000",
-  nc: "#1A4484",
-  hanhwa: "#EF563A",
-  lotte: "#092346",
-  kiwoom: "#801C26",
+const SCHEME_MAP: Record<TeamScheme, { name: string; style: string }> = {
+  kia: { name: "기아", style: "bg-team-kia" },
+  samsung: { name: "삼성", style: "bg-team-samsung" },
+  lg: { name: "LG", style: "bg-team-lg" },
+  doosan: { name: "두산", style: "bg-team-doosan" },
+  ssg: { name: "SSG", style: "bg-team-ssg" },
+  kt: { name: "KT", style: "bg-team-kt" },
+  nc: { name: "NC", style: "bg-team-nc" },
+  hanhwa: { name: "한화", style: "bg-team-hanhwa" },
+  lotte: { name: "롯데", style: "bg-team-lotte" },
+  kiwoom: { name: "키움", style: "bg-team-kiwoom" },
 };
 
-const teamNameMapping: { [key in TeamScheme]: string } = {
-  kia: 'KIA',
-  samsung: '삼성',
-  lg: 'LG',
-  doosan: '두산',
-  ssg: 'SSG',
-  kt: 'KT',
-  nc: 'NC',
-  hanhwa: '한화',
-  lotte: '롯데',
-  kiwoom: '키움',
-};
-
-const transformTeamName = (teamName: TeamScheme): string => {
-  return teamName.length >= 4 ? teamNameMapping[teamName] : teamName.toUpperCase();
+const transformTeamName = (teamName: TeamScheme): { name: string; style: string } => {
+  return SCHEME_MAP[teamName];
 };
 
 export interface PostType {
@@ -64,58 +51,53 @@ const Post = ({ post }: Props) => {
     likes
   } = post;
 
-  const homeTeamName = transformTeamName(home_team_name);
-  const awayTeamName = transformTeamName(away_team_name);
-
-  const homeTeamColor = teamColors[home_team_name];
-  const awayTeamColor = teamColors[away_team_name];
+  const homeTeam = transformTeamName(home_team_name);
+  const awayTeam = transformTeamName(away_team_name);
 
   return (
-<div className="ml-6 mr-6 mb-0 flex justify-center overflow-hidden bg-white">
-  <div className="w-full max-w-lg">
-    <div className="pt-3 pb-3 border-t-2 border-gray-300">
-      <div className="flex items-center">
-        <div 
-          className="p-2 text-white w-14 h-10"
-          style={{ 
-            backgroundColor: homeTeamColor,
-            borderRadius: '9999px 0 0 9999px',
-            paddingLeft: '1rem',
-            minWidth: '3.5rem',
-          }}
-        >
-          {homeTeamName}
+    <div className="ml-6 mr-6 mb-0 flex justify-center overflow-hidden bg-white">
+      <div className="w-full max-w-lg">
+        <div className="pt-3 pb-3 border-t-2 border-gray-300">
+          <div className="flex items-center">
+            <div
+              className={`p-2 text-white w-14 h-10 ${homeTeam.style}`}
+              style={{
+                borderRadius: '9999px 0 0 9999px',
+                paddingLeft: '1rem',
+                minWidth: '3.5rem',
+              }}
+            >
+              {homeTeam.name}
+            </div>
+            <div
+              className={`p-2 text-white w-14 h-10 ${awayTeam.style}`}
+              style={{
+                borderRadius: '0 9999px 9999px 0',
+                paddingRight: '1rem',
+                minWidth: '3.5rem',
+              }}
+            >
+              {awayTeam.name}
+            </div>
+            <div
+              className="pl-6 font-bold overflow-hidden w-80"
+              style={{
+                display: '-webkit-box',
+                WebkitBoxOrient: 'vertical',
+                WebkitLineClamp: 1,
+                textOverflow: 'ellipsis',
+              }}
+            >
+              {review_short}
+            </div>
+          </div>
+          <div className="flex">
+            <div>{comments}</div>
+            <div>{likes}</div>
+          </div>
         </div>
-        <div 
-          className="p-2 text-white  w-14 h-10"
-          style={{ 
-            backgroundColor: awayTeamColor,
-            borderRadius: '0 9999px 9999px 0',
-            paddingRight: '1rem',
-            minWidth: '3.5rem',
-          }}
-        >
-          {awayTeamName}
-        </div>
-        <div 
-            className="pl-6 font-bold overflow-hidden w-80" 
-            style={{ 
-            display: '-webkit-box', 
-            WebkitBoxOrient: 'vertical', 
-            WebkitLineClamp: 1, 
-            textOverflow: 'ellipsis'
-          }}
-        >
-          {review_short}
-        </div>
-      </div>
-      <div className="flex">
-        <div>{comments}</div>
-        <div>{likes}</div>
       </div>
     </div>
-  </div>
-</div>
   );
 }
 
