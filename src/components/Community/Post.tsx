ff86b610798d2f08abcc40db1d94c7df8b842e26
harry <tl9434@naver.com>
2024-08-14 22:44:14 +0900

    ⚡️ Post 컴포넌트 UI 적용
    Community 컴포넌트 코드 수정

--- src/components/Community/Post.tsx
+++ src/components/Community/Post.tsx
@@ -1,3 +1,11 @@
+import Badge from "../common/Badge";
+
+const dummyUser = [
+  { id: 1, user_name: 'Sabrina', cheer_team: 'kia' },
+  { id: 2, user_name: 'Redford', cheer_team: 'samsung' },
+  { id: 3, user_name: 'Malo', cheer_team: 'ssg' },
+];
+
 type TeamScheme =
   | "kia"
   | "samsung"
@@ -47,53 +55,67 @@ const Post = ({ post }: Props) => {
     home_team_name,
     away_team_name,
     review_short,
+    user_id,
     comments,
-    likes
+    likes,
   } = post;
 
   const homeTeam = transformTeamName(home_team_name);
   const awayTeam = transformTeamName(away_team_name);
 
+  const user = dummyUser.find(user => user.id === user_id);
+  const cheerTeam = user ? user.cheer_team : "";
+  const userName = user ? user.user_name : "Unknown";
+
+  const displayComments = comments >= 100 ? "99+" : comments;
+  const displayLikes = likes >= 100 ? "99+" : likes;
+
   return (
-    <div className="ml-6 mr-6 mb-0 flex justify-center overflow-hidden bg-white">
+    <div className="ml-6 mr-6 flex justify-center overflow-hidden bg-white">
       <div className="w-full max-w-lg">
-        <div className="pt-3 pb-3 border-t-2 border-gray-300">
+        <div className="pt-3 border-t-2 border-gray-300">
           <div className="flex items-center">
             <div
-              className={`p-2 text-white w-14 h-10 ${homeTeam.style}`}
-              style={{
-                borderRadius: '9999px 0 0 9999px',
-                paddingLeft: '1rem',
-                minWidth: '3.5rem',
-              }}
+              className={`p-2 text-white w-14 h-10 ${homeTeam.style} text-center rounded-l-full`}
+              style={{ minWidth: '3.5rem' }}
             >
               {homeTeam.name}
             </div>
             <div
-              className={`p-2 text-white w-14 h-10 ${awayTeam.style}`}
-              style={{
-                borderRadius: '0 9999px 9999px 0',
-                paddingRight: '1rem',
-                minWidth: '3.5rem',
-              }}
+              className={`p-2 text-white w-14 h-10 ${awayTeam.style} text-center rounded-r-full`}
+              style={{ minWidth: '3.5rem' }}
             >
               {awayTeam.name}
             </div>
-            <div
-              className="pl-6 font-bold overflow-hidden w-80"
-              style={{
-                display: '-webkit-box',
-                WebkitBoxOrient: 'vertical',
-                WebkitLineClamp: 1,
-                textOverflow: 'ellipsis',
-              }}
-            >
+            <div className="pl-6 font-bold overflow-hidden w-80 text-ellipsis whitespace-nowrap">
               {review_short}
             </div>
           </div>
-          <div className="flex">
-            <div>{comments}</div>
-            <div>{likes}</div>
+          <div className="flex justify-between items-center mt-2">
+            <div className="flex items-center font-normal text-gray-500">
+              <div className="flex items-center justify-center" style={{ width: '3rem', height: '3rem' }}>
+                <Badge scheme={cheerTeam as TeamScheme} />
+              </div>
+              <div className="ml-2 text-sm">{userName}</div>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="flex items-center w-16 h-10">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="22" fill="none">
+                  <path fill="#5ACAD2" d="m5.062 17.51.67.336a.75.75 0 0 0-.256-.962l-.414.625Zm-1.373 2.736-.67-.337.67.337Zm.661.7.335.672-.335-.671Zm3.943-1.962.209-.72a.75.75 0 0 0-.543.05l.334.67Zm14.957-8.706c0 4.59-4.128 8.529-10.75 8.529v1.5c7.185 0 12.25-4.369 12.25-10.029h-1.5ZM12.5 1.75c6.622 0 10.75 3.94 10.75 8.528h1.5C24.75 4.618 19.685.25 12.5.25v1.5ZM1.75 10.278C1.75 5.69 5.878 1.75 12.5 1.75V.25C5.315.25.25 4.619.25 10.278h1.5Zm3.726 6.606C3.099 15.31 1.75 12.926 1.75 10.278H.25c0 3.199 1.642 6.03 4.397 7.856l.829-1.25Zm-1.117 3.698 1.373-2.736-1.34-.673-1.373 2.736 1.34.673Zm-.343-.307a.273.273 0 0 1 .304.052.233.233 0 0 1 .04.255l-1.341-.673a1.268 1.268 0 0 0 .21 1.448 1.23 1.23 0 0 0 1.456.261l-.669-1.343Zm3.943-1.962-3.943 1.962.669 1.343 3.942-1.962-.668-1.343Zm4.541.494c-1.457 0-2.796-.194-3.998-.543l-.419 1.44c1.346.392 2.828.603 4.417.603v-1.5Z"/>
+                </svg>
+                <span className="text-[#5ACAD2] ml-2">
+                  {displayComments}
+                </span>
+              </div>
+              <div className="flex items-center w-16 h-10">
+                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="22" fill="none">
+                  <path stroke="#DC7B7C" stroke-linejoin="round" stroke-width="1.5" d="M1.01 8.324c-.213-5.372 2.815-7.06 5.138-7.305C8.472.774 10.644 2.952 11.5 5.53c.856-2.579 3.012-4.67 5.352-4.512 2.339.157 5.351 1.933 5.137 7.305-.208 5.226-3.861 8.621-9.974 12.363a.988.988 0 0 1-1.03 0C4.872 16.945 1.219 13.55 1.01 8.324Z"/>
+                </svg>
+                <span className="text-[#DC7B7C] ml-2">
+                  {displayLikes}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Community/Community.tsx
+++ src/pages/Community/Community.tsx
@@ -1,5 +1,4 @@
 import Post, { PostType } from "../../components/Community/Post";
-import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const dummyPost: PostType[] = [
@@ -8,53 +7,126 @@ const dummyPost: PostType[] = [
     match_date: "2024.01.01",
     home_team_name: "kia",
     away_team_name: "samsung",
-    review_short: "재미있었다 재미있었다 재미있었다 재미있었다 재미있었다 ",
+    review_short: "재미있었다",
     user_id: 1,
-    likes: 1,
-    comments: 0,
+    likes: 0,
+    comments: 1,
   },
   {
     id: 2,
     match_date: "2024.01.02",
     home_team_name: "lg",
     away_team_name: "doosan",
-    review_short: "즐거웠다 즐거웠다 즐거웠다 즐거웠다 즐거웠다 즐거웠다 ",
+    review_short: "재미있었다 재미있었다",
     user_id: 2,
-    likes: 2,
+    likes: 1,
+    comments: 2,
+  },
+  {
+    id: 3,
+    match_date: "2024.01.03",
+    home_team_name: "ssg",
+    away_team_name: "kt",
+    review_short: "재미있었다 재미있었다 재미있었다",
+    user_id: 3,
+    likes: 20,
+    comments: 3,
+  },
+  {
+    id: 4,
+    match_date: "2024.01.04",
+    home_team_name: "nc",
+    away_team_name: "hanhwa",
+    review_short: "재미있었다 재미있었다 재미있었다 재미있었다",
+    user_id: 1,
+    likes: 300,
+    comments: 4,
+  },
+  {
+    id: 5,
+    match_date: "2024.01.05",
+    home_team_name: "lotte",
+    away_team_name: "kiwoom",
+    review_short: "재미있었다 재미있었다 재미있었다 재미있었다 재미있었다",
+    user_id: 2,
+    likes: 4000,
+    comments: 5,
+  },
+  {
+    id: 1,
+    match_date: "2024.01.01",
+    home_team_name: "samsung",
+    away_team_name: "kia",
+    review_short: "재미있었다",
+    user_id: 3,
+    likes: 1,
     comments: 0,
   },
-]
+  {
+    id: 2,
+    match_date: "2024.01.02",
+    home_team_name: "doosan",
+    away_team_name: "lg",
+    review_short: "재미있었다 재미있었다",
+    user_id: 1,
+    likes: 2,
+    comments: 1,
+  },
+  {
+    id: 3,
+    match_date: "2024.01.03",
+    home_team_name: "kt",
+    away_team_name: "ssg",
+    review_short: "재미있었다 재미있었다 재미있었다",
+    user_id: 2,
+    likes: 3,
+    comments: 20,
+  },
+  {
+    id: 4,
+    match_date: "2024.01.04",
+    home_team_name: "hanhwa",
+    away_team_name: "nc",
+    review_short: "재미있었다 재미있었다 재미있었다 재미있었다",
+    user_id: 3,
+    likes: 4,
+    comments: 300,
+  },
+  {
+    id: 5,
+    match_date: "2024.01.05",
+    home_team_name: "kiwoom",
+    away_team_name: "lotte",
+    review_short: "재미있었다 재미있었다 재미있었다 재미있었다 재미있었다",
+    user_id: 1,
+    likes: 5,
+    comments: 4000,
+  },
+];
 
 const Community = () => {
   const location = useLocation();
-  const [currentPage, setCurrentPage] = useState('');
-
-  useEffect(() => {
-    if (location.pathname === '/posts') {
-      setCurrentPage('community');
-    } else if (location.pathname === '/archive') {
-      setCurrentPage('archive');
-    }
-  }, [location]);
+  const isCommunity = location.pathname === '/posts';
+  const isDiary = location.pathname === '/archive';
 
   return (
-    <div>
+    <div className="mb-24">
       <div className="m-10 flex justify-center">
-        <button className={`w-20 h-12 ${currentPage === 'community' ? 'underline underline-offset-4' : ''}`}>
+        <button className={`w-20 h-12 ${isCommunity ? 'underline underline-offset-4' : ''}`}>
           <span className="font-black">커뮤니티</span>
         </button>
-        <button className={`w-20 h-12 ${currentPage === 'diary' ? 'underline underline-offset-4' : ''}`}>
+        <button className={`w-20 h-12 ${isDiary ? 'underline underline-offset-4' : ''}`}>
           <span className="font-black">일기</span>
         </button>
       </div>
-      {
-        dummyPost.map((post) => (
+      <div>
+        {dummyPost.slice().reverse().map((post) => (
           <Post 
             key={post.id}
             post={post}
           />
-        ))
-      }
+        ))}
+      </div>
     </div>
   );
 };
