1df322670e4841a6a05b7178204104252ce961d5
harry <tl9434@naver.com>
2024-08-16 15:12:01 +0900

    ⚡️ PostDetail 본문 구현

--- src/components/Community/PostDetail.tsx
+++ src/components/Community/PostDetail.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { PostType } from "./Post";
 import { transformTeamName } from "./Post";
 import { dummyUser } from "../../pages/Community/dummyPostUser";
@@ -12,7 +11,6 @@ interface Props {
 const PostDetail = ({ postDetail }: Props) => {
   
   const {
-    id,
     user_id,
     review_short,
     match_date, 
@@ -22,6 +20,7 @@ const PostDetail = ({ postDetail }: Props) => {
     home_team_name, 
     away_team_name,
     review_long,
+    photo,
   } = postDetail;
 
   const homeTeam = transformTeamName(home_team_name);
@@ -31,18 +30,17 @@ const PostDetail = ({ postDetail }: Props) => {
   const cheerTeam = user ? user.cheer_team : undefined;
   const userName = user ? user.user_name : "Unknown";
 
+  const getPhotoSrc = (id: number) => {
+    return `https://picsum.photos/id/${id}/600/600`;
+  };
+
   return (
     <div className="ml-6 mr-6 flex justify-center overflow-hidden bg-white">
       <div className="w-full max-w-lg">
         <div className="pt-3 border-b-2 border-gray-300">
-          <div className="flex items-center">
-            <div className="pl-2 font-bold">
+          <div className="flex items-start">
+            <div className="pl-2 pr-2 font-bold">
               {review_short}
-            </div>
-            <div className="ml-auto text-right">
-              <div className="text-sm text-gray-400 font-thin">
-                {match_date}
-              </div>
             </div>
           </div>
           <div className="flex justify-between items-center mt-2">
@@ -52,6 +50,43 @@ const PostDetail = ({ postDetail }: Props) => {
               </div>
               <div className="ml-1 text-sm">{userName}</div>
             </div>
+            <div className="text-sm text-gray-400 font-thin">
+                {match_date}
+              </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-1 mt-2 pb-3 border-b border-gray-300 text-sm">
+          <div className="flex items-start">
+            <p className="text-gray-500 font-thin min-w-[4rem]">경기 날짜</p>
+            <p className="font-bold min-w-[4rem]">{match_date}</p>
+          </div>
+          <div className="flex items-center">
+            <p className="text-gray-500 font-thin min-w-[4rem]">경기 결과</p>
+            <p className="font-bold min-w-[4rem]">{result}</p>
+          </div>
+          <div className="flex items-center">
+            <p className="text-gray-500 font-thin min-w-[4rem]">경기장</p>
+            <p className="font-bold min-w-[4rem]">{stadium}</p>
+          </div>
+          <div className="flex items-center">
+            <p className="text-gray-500 font-thin min-w-[4rem]">날씨</p>
+            <p className="font-bold min-w-[4rem]">{weather}</p>
+          </div>
+          <div className="flex items-center">
+            <p className="text-gray-500 font-thin min-w-[4rem]">홈</p>
+            <p className="font-bold min-w-[4rem]">{homeTeam.name}</p>
+          </div>
+          <div className="flex items-center">
+            <p className="text-gray-500 font-thin min-w-[4rem]">어웨이</p>
+            <p className="font-bold min-w-[4rem]">{awayTeam.name}</p>
+          </div>
+        </div>
+        <div className="mt-6 mb-16">
+          <div>
+            <div className="pb-4">
+              <img src={getPhotoSrc(photo)} alt="Post Photo" />
+            </div>
+            <p>{review_long}</p>
           </div>
         </div>
       </div>
