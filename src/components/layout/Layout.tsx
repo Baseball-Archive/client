a17760d03dee1a1707c91bb1093ce4689a25472d
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-08-16 10:51:32 +0900

    :lipstick: 본문 간격 조정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,24 +9,23 @@ const NAV_DEFAULT =
 
 const CONTENT_XL = "xl:w-2/4 xl:left-1/4";
 const CONTENT_MD = "md:w-[calc(100%-100px)] md:left-[100px]";
-const CONTENT_DEFAULT = "absolute top-0 pt-[60px] border w-100 left-0 w-full";
+const CONTENT_DEFAULT = "absolute top-0 pt-[60px] w-100 left-0 w-full px-3";
 
 const Layout = () => {
   return (
     <>
-      <div className="fixed top-0 left-0 w-full z-10 md:hidden">
+      <div className="fixed left-0 top-0 z-10 w-full md:hidden">
         <Header />
       </div>
       <div className={`${NAV_DEFAULT} ${NAV_XL} ${NAV_MD}`}>
         <Navigation />
       </div>
-      <div
-        className={`${CONTENT_DEFAULT} ${CONTENT_XL} ${CONTENT_MD}`}> 
-        <div className="max-w-screen-md mx-auto my-0">
+      <div className={`${CONTENT_DEFAULT} ${CONTENT_XL} ${CONTENT_MD}`}>
+        <div className="mx-auto my-0 max-w-screen-md">
           <Outlet />
         </div>
       </div>
     </>
   );
-}; 
+};
 export default Layout;
