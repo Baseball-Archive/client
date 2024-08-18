327e58dba81c671ea6aea43e5a130dda10e8c35b
harry <tl9434@naver.com>
2024-08-18 15:37:41 +0900

    🐛 AddPost 컴포넌트 이름 수정

--- src/pages/Community/AddPost.tsx
+++ src/pages/Community/AddPost.tsx
@@ -1,6 +1,6 @@
 import PostInfo from "../../components/Community/AddPost/PostInfo";
 
-const AddArchive = () => {
+const AddPost = () => {
   return (
     <div className="container mb-6 flex justify-center overflow-auto bg-white">
       <div className="flex w-full max-w-md flex-col overflow-auto">
@@ -10,4 +10,4 @@ const AddArchive = () => {
   );
 };
 
-export default AddArchive;
+export default AddPost;
