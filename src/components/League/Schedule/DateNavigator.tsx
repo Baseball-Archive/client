d90e283187a94abdc95741431690b0596722d919
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-08-23 18:13:53 +0900

    💄UI 수정

--- src/components/League/Schedule/DateNavigator.tsx
+++ src/components/League/Schedule/DateNavigator.tsx
@@ -1,10 +1,20 @@
-const DateNavigator = () => {
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
+import dayjs from 'dayjs';
+
+interface Props {
+  date: string;
+}
+
+const DateNavigator = ({ date }: Props) => {
   return (
-    <>
-      <h1 className="border-b border-black pb-3 text-center text-2xl font-bold">
-        2024.00.00(요)
-      </h1>
-    </>
+    <div className="flex justify-center bg-gray-100 py-4 text-center text-2xl font-bold">
+      <ChevronLeftIcon
+        className="size-8 text-gray-500"
+        onClick={() => dayjs(date).add(1, 'day')}
+      />
+      <h1>{date}</h1>
+      <ChevronRightIcon className="size-8 text-gray-500" />
+    </div>
   );
 };
 export default DateNavigator;
